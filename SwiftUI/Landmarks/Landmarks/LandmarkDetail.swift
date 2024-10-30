2ee55d2022e851396a30763b095123099d2f6ad6
est22 <anlia.seoul@gmail.com>
2024-10-30 16:55:45 +0900

    ✨ feat: present views via navigation

---
+++ SwiftUI/Landmarks/Landmarks/LandmarkDetail.swift
@@ -0,0 +1,37 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Lia An on 10/30/24.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate).frame(height: 300)
+            CircleImage(image: landmark.image).offset(y:-130).padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title.bold())
+                
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }.font(.subheadline)
+                    .foregroundStyle(.secondary)
+                Divider()
+                Text("About \(landmark.name)").font(.title2)
+                Text(landmark.description)
+            }.padding()
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: landmarks[1])
+}
---
+++ SwiftUI/Landmarks/Landmarks/LandmarkRow.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Lia An on 10/30/24.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image.resizable().frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[1])
+}
