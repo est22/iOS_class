3503f736feb80951b03cfa0d7f2b2f440ab5ec23
est22 <anlia.seoul@gmail.com>
2024-11-06 15:08:49 +0900

    ✨ feat: add signUp

--- UIKit/YangpaMarket/YangpaMarket/LoginViewController.swift
+++ UIKit/YangpaMarket/YangpaMarket/LoginViewController.swift
@@ -19,6 +19,29 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func actSignUp(_ sender: Any) {
+        let parameters: Parameters = ["userName":txtId.text!, "password":txtPassword.text!]
+        let endPoint = "\(host)/members/sign-up"
+        let alamo = AF.request(endPoint, method: .post, parameters: parameters)
+        alamo.responseDecodable(of: SignUp.self){ response in
+            switch response.result {
+            case .success(let signUp):
+                let alert = UIAlertController(title: "회원가입", message: signUp.message, preferredStyle: .alert)
+                let action = UIAlertAction(title: "확인", style: .default) { _ in
+                    self.txtId.text = ""
+                    self.txtPassword.text = ""
+                }
+                alert.addAction(action)
+                self.present(alert, animated: true)
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    @IBAction func actLogin(_ sender: Any) {
+    }
+    
 
     /*
     // MARK: - Navigation
