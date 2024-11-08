a2192f8ef58cdb5a898af27c2f6f60ac4d468a10
est22 <anlia.seoul@gmail.com>
2024-11-08 12:05:52 +0900

    ✨ feat: add navigationBar

--- SwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUI/BookList.swift
+++ SwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUI/BookList.swift
@@ -13,14 +13,21 @@ struct BookList: View {
     @StateObject var bookFinder = BookFinder() // 2
     
     var body: some View {
-        VStack{
-            SearchBar(searchText: $query) {
-                page = 1
-                bookFinder.search(query: query, at: page) // 2, 3
+        NavigationSplitView {
+            VStack{
+                SearchBar(searchText: $query) {
+                    page = 1
+                    bookFinder.search(query: query, at: page) // 2, 3
+                }
+                List(bookFinder.books) { book in
+                    BookRow(book: book)
+                }
+                .listStyle(.plain)
+                .navigationTitle("책 검색")
+                .navigationBarTitleDisplayMode(.inline)
             }
-            List(bookFinder.books) { book in
-                BookRow(book: book)
-            }
+        } detail: {
+            
         }
     }
 }
