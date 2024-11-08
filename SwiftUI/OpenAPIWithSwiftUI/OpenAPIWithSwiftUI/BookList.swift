185b4c5005099ea7ae3b740708d3420a8dacfc73
est22 <anlia.seoul@gmail.com>
2024-11-08 11:49:00 +0900

    ✨ feat: show bookList

---
+++ SwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUI/BookList.swift
@@ -0,0 +1,27 @@
+//
+//  BookList.swift
+//  OpenAPIWithSwiftUI
+//
+//  Created by Lia An on 11/8/24.
+//
+
+import SwiftUI
+
+struct BookList: View {
+    @State private var query: String = "" // 1
+    @State var page = 1 // 3
+    @StateObject var bookFinder = BookFinder() // 2
+    
+    var body: some View {
+        VStack{
+            SearchBar(searchText: $query) {
+                page = 1
+                bookFinder.search(query: query, at: page) // 2, 3
+            }
+        }
+    }
+}
+
+#Preview {
+    BookList()
+}
