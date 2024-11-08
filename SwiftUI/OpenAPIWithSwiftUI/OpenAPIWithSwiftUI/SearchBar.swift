bc97559fc3cefc958c99d7bc2960086fee8b3211
est22 <anlia.seoul@gmail.com>
2024-11-08 10:35:38 +0900

    ✨ feat: cancel button appear/disappear when the searchBar tapped

--- SwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUI/SearchBar.swift
+++ SwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUI/SearchBar.swift
@@ -9,14 +9,26 @@ import SwiftUI
 
 struct SearchBar: View {
     @Binding var searchText: String
+    @State var isEditing: Bool = false
+    
     var body: some View {
         HStack {
             TextField("검색어를 입력하세요", text: $searchText).padding()
         .background(Color(.systemGray6))
             .clipShape(.rect(cornerRadius: 18))
             .padding(.horizontal, 10)
-        
-
+            .onTapGesture {
+                isEditing = true
+            }.animation(.easeInOut, value: isEditing)
+            if isEditing {
+                Button {
+                    isEditing = false
+                } label: {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 15)
+                .transition(.move(edge: .trailing))
+            }
         }
     }
 }
