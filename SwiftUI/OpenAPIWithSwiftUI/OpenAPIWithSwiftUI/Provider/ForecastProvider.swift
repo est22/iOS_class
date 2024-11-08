aa3b8893c4ee8cc9366d6a35b0fa6dcfaab62f1d
est22 <anlia.seoul@gmail.com>
2024-11-08 16:10:08 +0900

    ✨ feat: request/require forecast via weatherAPI

---
+++ SwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUI/Provider/ForecastProvider.swift
@@ -0,0 +1,37 @@
+//
+//  ForecastProvider.swift
+//  OpenAPIWithSwiftUI
+//
+//  Created by Lia An on 11/8/24.
+//
+
+import Foundation
+import Alamofire
+
+class ForecastProvider: ObservableObject {
+    @Published var list: [Forecast]?
+    let appid = "0d62c45ebe5fff9fca2ec0bfda3a394b"
+    let endPoint = "api.openweathermap.org/data/2.5/forecast?"
+    
+    func getForecast(city: String) {
+        let parameters: [String: Any] = ["q":city, "appid":appid, "lang":"kr", "units":"metric"]
+        AF.request(endPoint, method: .get, parameters: parameters)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: Root.self) { response in
+                switch response.result {
+                case .success(let forecasts):
+                    self.list = forecasts.list
+                    print(forecasts.list[0])
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            
+        }
+        
+    }
+    
+    
+    
+    
+}
