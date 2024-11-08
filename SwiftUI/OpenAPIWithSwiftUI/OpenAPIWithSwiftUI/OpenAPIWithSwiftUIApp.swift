586efc9b9c813b0976dacec05dbd381e0cbe368e
est22 <anlia.seoul@gmail.com>
2024-11-08 10:11:34 +0900

    🎉 init: create project

---
+++ SwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAPIWithSwiftUIApp.swift
+//  OpenAPIWithSwiftUI
+//
+//  Created by Lia An on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenAPIWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/OpenAPIWithSwiftUI/OpenAPIWithSwiftUI/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  OpenAPIWithSwiftUI
+//
+//  Created by Lia An on 11/8/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}
