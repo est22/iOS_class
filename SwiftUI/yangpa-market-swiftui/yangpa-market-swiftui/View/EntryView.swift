ec316d519ed104e39e47284f6aeb36a21609be44
est22 <anlia.seoul@gmail.com>
2024-11-15 15:02:51 +0900

    ✨ feat: create EntryView using MemberViewModel

---
+++ SwiftUI/yangpa-market-swiftui/yangpa-market-swiftui/View/EntryView.swift
@@ -0,0 +1,26 @@
+//
+//  EntryView.swift
+//  yangpa-market-swiftui
+//
+//  Created by Lia An on 11/15/24.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    @EnvironmentObject var memberVM: MemberViewModel
+    
+    var body: some View {
+        
+        if memberVM.isLoggedIn {
+            MainView()
+        } else {
+            LoginView(userID:"lia", password: "1234").transition(.move(edge:.bottom))
+        }
+    }
+}
+
+#Preview {
+    let memberVM = MemberViewModel()
+    EntryView().environmentObject(memberVM)
+}
