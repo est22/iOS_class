143901b6bb5bbac7eb73a6d54ce62da54884cad4
est22 <anlia.seoul@gmail.com>
2024-11-15 11:36:45 +0900

    🎉 init: create project and set structure(MVVM)

---
+++ SwiftUI/yangpa-market-swiftui/yangpa-market-swiftui/yangpa_market_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  yangpa_market_swiftuiApp.swift
+//  yangpa-market-swiftui
+//
+//  Created by Lia An on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct yangpa_market_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
