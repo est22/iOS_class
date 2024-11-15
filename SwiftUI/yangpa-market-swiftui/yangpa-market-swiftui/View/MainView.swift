eb0762270001b70ff4288e3419318054f5fd082e
est22 <anlia.seoul@gmail.com>
2024-11-15 15:04:33 +0900

    ✨ feat: create MainView with Logout button

---
+++ SwiftUI/yangpa-market-swiftui/yangpa-market-swiftui/View/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  yangpa-market-swiftui
+//
+//  Created by Lia An on 11/15/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @EnvironmentObject var memberVM: MemberViewModel
+    
+    var body: some View {
+        Text("MainView")
+        Button("로그아웃") {
+            memberVM.isLoggedIn = false
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
