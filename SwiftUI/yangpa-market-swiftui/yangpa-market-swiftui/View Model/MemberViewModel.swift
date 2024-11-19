70ca93b1e2ee4c925ac68d60f7105aeb7fbcc38b
est22 <anlia.seoul@gmail.com>
2024-11-19 12:13:10 +0900

    ✨ feat: join feature(sign-up)

--- SwiftUI/yangpa-market-swiftui/yangpa-market-swiftui/View Model/MemberViewModel.swift
+++ SwiftUI/yangpa-market-swiftui/yangpa-market-swiftui/View Model/MemberViewModel.swift
@@ -14,6 +14,7 @@ import SVProgressHUD
 class MemberViewModel: ObservableObject {
     @Published var isLoggedIn = false
     @Published var isLoginError = false
+    @Published var isJoinSuccess = false
     @Published var message = ""
     let endPoint = "http://localhost:3000"
     
@@ -25,10 +26,10 @@ class MemberViewModel: ObservableObject {
         // request
         AF.request(url, method: .post, parameters: params)
             .response { response in // response handling
-            if let statusCode = response.response?.statusCode {
-                // error handling per statusCode
+                if let statusCode = response.response?.statusCode {
+                    // error handling per statusCode
                     switch statusCode {
-                    // case 1
+                        // case 1
                     case 200..<300:
                         if let data = response.data {
                             // data decode error handling
@@ -40,17 +41,69 @@ class MemberViewModel: ObservableObject {
                                 UserDefaults.standard.set(signIn.token, forKey: "token")
                                 UserDefaults.standard.set(signIn.member.userName, forKey: "userName")
                                 UserDefaults.standard.set(self.isLoggedIn, forKey: "isLoggedIn")
-
+                                
                             } catch let error {
                                 self.isLoginError = true
                                 self.message =  error.localizedDescription
                             }
                             
                         }
-                    // case 2
+                        // case 2
                     case 300..<600:
                         self.isLoginError = true
                         
+                        if let data = response.data {
+                            do {
+                                let apiError = try JSONDecoder().decode(APIError.self, from: data)
+                                self.isJoinSuccess = true
+                                self.message = apiError.message
+                            } catch let error {
+                                self.message = error.localizedDescription
+                            }
+                        }
+                        // case default
+                    default:
+                        self.isLoginError = true
+                        self.message = "알 수 없는 에러가 발생했습니다."
+                        
+                    }
+                }
+                
+            }
+        SVProgressHUD.dismiss()
+    }
+    
+    
+    func join(userName: String, password: String) {
+        SVProgressHUD.show()
+        
+        let url = "\(endPoint)/members/sign-up"
+        let params:Parameters = ["userName":userName, "password":password]
+        // request
+        AF.request(url, method: .post, parameters: params)
+            .response { response in // response handling
+                
+                if let statusCode = response.response?.statusCode {
+                    self.isJoinSuccess = true
+                    // error handling per statusCode
+                    switch statusCode {
+                    // case 1
+                    case 200..<300:
+                        if let data = response.data {
+                            // data decode error handling
+                            do {
+                                let signUp = try JSONDecoder().decode(SignUp.self, from: data)
+                                // change user status to logged in
+                                self.message = signUp.message
+                                
+                            } catch let error {
+                                
+                                self.message =  error.localizedDescription
+                            }
+                            
+                        }
+                        // case 2
+                    case 300..<600:
                         if let data = response.data {
                             do {
                                 let apiError = try JSONDecoder().decode(APIError.self, from: data)
@@ -59,15 +112,15 @@ class MemberViewModel: ObservableObject {
                                 self.message = error.localizedDescription
                             }
                         }
-                    // case default
+                        
+                        // case default
                     default:
-                        self.isLoginError = true
                         self.message = "알 수 없는 에러가 발생했습니다."
-                    
-                }
+                        
+                    }
                 }
                 
-        }
+            }
         SVProgressHUD.dismiss()
     }
 }
