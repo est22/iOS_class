8d26afecc4ff3cbfeff87dd4394aa324dd577028
est22 <anlia.seoul@gmail.com>
2024-11-21 15:32:25 +0900

    ✨ feat: notification test in UIKit

---
+++ UIKit/notification-test/notification-test/AppDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppDelegate.swift
+//  notification-test
+//
+//  Created by Lia An on 11/21/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
+        let option = UNAuthorizationOptions(arrayLiteral: [.alert, .sound])
+        UNUserNotificationCenter.current().requestAuthorization(options: option) {
+            granted, error in
+            if granted {
+                
+            }
+        }
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let info = notification.request.content.userInfo
+        print(info["name"] ?? "")
+        completionHandler([.banner, .sound])
+    }
+    
+    // 유저 알람 클릭 액션 -> 앱으로 접속
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let info = response.notification.request.content.userInfo
+        print(info["name"] ?? "")
+        completionHandler()
+        
+    }
+        
+        
+
+
+}
+
---
+++ UIKit/notification-test/notification-test/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  notification-test
+//
+//  Created by Lia An on 11/21/24.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func actInterval(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "Interval Notification"
+        content.subtitle = "This is a subtitle"
+        content.body = "설정하신 인터벌 노티피케이션을 알려드립니다."
+        content.userInfo = ["name": "정년이"]
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) {
+            (error) in
+            if let error {
+                print("알림설정에 실패했습니다.")
+            }
+            print("알림설정에 성공했습니다.")
+        }
+    }
+    
+    @IBAction func actCalendar(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "Calendar Notification"
+        content.subtitle = "This is a subtitle"
+        content.body = "설정하신 캘린더 노티피케이션을 알려드립니다."
+        content.userInfo = ["name": "정년이"]
+        
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        
+        
+        var dateComponents = DateComponents()
+    
+        dateComponents.hour = 14
+        dateComponents.minute = 56
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
+        
+        
+        
+    }
+    
+    @IBAction func actLocation(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "Location Notification"
+        content.subtitle = "This is a subtitle"
+        content.body = "설정하신 위치 기반 노티피케이션을 알려드립니다."
+        content.userInfo = ["name": "정년이"]
+        
+        let center = CLLocationCoordinate2D(latitude: 37, longitude: -122)
+        let region = CLCircularRegion(center: center, radius: 1000, identifier: "iOS study")
+        
+        region.notifyOnEntry = true
+        region.notifyOnExit = true
+        
+        let trigger = UNLocationNotificationTrigger(region: region, repeats: true)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request)
+        
+    }
+}
+
